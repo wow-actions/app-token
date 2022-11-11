89f4c49790bc45a25abc192965fdd8510f0431d1
bubkoo <bubkoo.wy@gmail.com>
2022-11-11 11:14:22 +0800

    chore: release config

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import * as util from './util'
 
 export = (app: Probot) => {
   try {
-    app.onAny((context) => {
-      // eslint-disable-next-line no-console
-      console.log(`>>>>>>>>>>>>>>> event: ${context.name} <<<<<<<<<<<<<<<<`)
-    })
+    // app.onAny((context) => {
+    //   // eslint-disable-next-line no-console
+    //   console.log(`>>>>>>>>>>>>>>> event: ${context.name} <<<<<<<<<<<<<<<<`)
+    // })
     app.on('workflow_run', async (context) => {
       const action = context.payload.action
       if (action !== 'completed') {
